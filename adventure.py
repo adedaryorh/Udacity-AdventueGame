86ac25fbdfe4840b45c4fe66e921628e51578e46
ADEDAYO A. ADEYEMI <64752771+adedaryorh@users.noreply.github.com>
2023-02-04 08:22:50 +0100

    edititng

--- adventure.py
+++ adventure.py
@@ -1,114 +1,177 @@
-import random
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# You can use this workspace to write and submit your adventure game project.
+
 import time
+import random
 import sys
 import unicodedata
-
-#This funtions prints out all messages when invoked.
-
-
-def print_pause(msg_to_print):
-    print(msg_to_print)
-    time.sleep(2)
+import enum
 
 
-def intro(item, option):
-    print_pause("You find yourself standing in an open field, filled "
-                "with grass and yellow wildflowers.\n")
-    print_pause("Rumor has it that a " + option + " is somewhere around "
-                "here, and has been terrifying the nearby village.\n")
-    print_pause("In front of you is a house.\n")
-    print_pause("To your right is a dark cave.\n")
-    print_pause("In your hand you hold your trusty (but not very "
-                "effective) dagger.\n")
+class Color(enum.Enum):
+
+    red = "\033[91m"
+    purple = "\033[95m"
+    blue = "\033[94m"
+    cyan = "\033[96m"
+    green = "\033[92m"
+    black = "\033[0m"
+    bold = "\033[1m"
+    underline = "\033[4m"
+
+    @classmethod
+    def get_color(cls):
+        return random.choice([color.value for color in cls])
 
 
-def cave(item, option):
-    if "sword" in item:
-        print_pause("\nYou peer cautiously into the cave.")
-        print_pause("\nYou've been here before, and gotten all"
-                    " the good stuff. It's just an empty cave"
-                    " now.")
-        print_pause("\nYou walk back to the field.\n")
-    else:
-        print_pause("\nYou peer cautiously into the cave.")
-        print_pause("\nIt turns out to be only a very small cave.")
-        print_pause("\nYour eye catches a glint of metal behind a "
-                    "rock.")
-        print_pause("\nYou have found the magical Spear of Zeus!")
-        print_pause("\nYou discard your silly old dagger and take "
-                    "the sword with you.")
-        print_pause("\nYou walk back out to the field.\n")
-        item.append("sword")
-    field(item, option)
+def print_pause(message, delay=2):
+    print(Color.get_color() + message)
+    time.sleep(delay)
 
 
-def house(item, option):
-    print_pause("\nYou approach the door of the house.")
-    print_pause("\nYou are about to knock when the door "
-                "opens and out steps a " + option + ".")
-    print_pause("\nEep! This is the " + option + "'s house!")
-    print_pause("\nThe " + option + " attacks you!\n")
-    if "sword" not in item:
-        print_pause("You feel a bit under-prepared for this, "
-                    "what with only having a tiny dagger.\n")
+# def print_pause(message_to_print, sleep_time=2):
+# print(message_to_print)
+# time.sleep(sleep_time)
+
+
+def valid_input(message_to_print, options):
     while True:
-        playerChoice = input("Would you like to (1) fight or (2) "
-                        "run away?")
-        if playerChoice == "1":
-            if "sward" in item:
-                print_pause("\nAs the " + option + " moves to attack, "
-                            "you unsheath your new sword.")
-                print_pause("\nThe Sword of Ogoroth shines brightly in "
-                            "your hand as you brace yourself for the "
-                            "attack.")
-                print_pause("\nBut the " + option + "takes one look at "
-                            "your shiny new toy and runs away!")
-                print_pause("\nYou have rid the town of the " + option +
-                            ". You are victorious!\n")
-            else:
-                print_pause("\nYou do your best...")
-                print_pause("but your dagger is no match for the "
-                            + option + ".")
-                print_pause("\nYou have been defeated!\n")
-            play_again()
-            break
-        if playerChoice == "2":
-            print_pause("\nYou run back into the field. "
-                        "\nLuckily, you don't seem to have been "
-                        "followed.\n")
-            field(item, option)
-            break
+        response = input(message_to_print).lower()
+        if response in options:
+            return response
+        print("Wrong expected input")
 
-
-def field(item, option):
-    print_pause("Enter 1 to knock on the door of the house.")
-    print_pause("Enter 2 to peer into the cave.")
-    print_pause("What would you like to do?")
-    while True:
-        firstChoice = input("(Please enter 1 or 2.)\n")
-        if firstChoice == "1":
-            house(item, option)
-            break
-        elif firstChoice == "2":
-            cave(item, option)
-            break
 
 def play_again():
-    again = input("Would you like to play again? (y/n)").lower()
-    if again == "y":
-        print_pause("\n\n\nExcellent! Restarting the game ...\n\n\n")
-        play_game()
-    elif again == "n":
-        print_pause("\n\n\nThanks for playing! See you next time.\n\n\n")
-    else:
+    output_option3 = valid_input(
+        "Would you like to play again? (y/n)", options=["y", "n"]
+    )
+    if output_option3 == "y":
+        print_pause("Excellent! Restarting the game ...")
+    elif output_option3 == "n":
+        print_pause("Thanks for playing! See you next time.")
+        exit(0)
+
+
+def runAction():
+    print_pause("You run back into the field. Luckily, "
+                "you don't seem to have been followed.\n", 2)
+    # play_game
+
+
+def fightAction(fight_texts):
+    for text in fight_texts:
+        print_pause(text)
+    print_pause("\n")
+
+
+def fight_choice(fight_texts):
+    output_option2 = valid_input(
+        "Would you like to do fightAction OR runAction ?\n", ["1", "2"]
+    )
+    if output_option2 == "1":
+        fightAction(fight_texts)
+        return True
+    elif output_option2 == "2":
+        runAction()
+        return False
+
+
+def house(creatures, weapon):
+    print_pause("\nYou approach the door of the house.")
+    print_pause(
+        "\nYou are about to knock when the door opens and out steps a "
+        + creatures
+        + "."
+    )
+    print_pause("\nEep! This is the " + creatures + "'s house!")
+    print_pause("\nThe " + creatures + " attacks you!\n")
+    if weapon == "badKnife":
+        print_pause("You feel a bit under-prepared for this, "
+                    "what with only having a tiny dagger.")
+        playerChoice = fight_choice(
+            [
+                "You do your best...",
+                "but your dagger is no match for the "
+                + creatures
+                + ".You have been defeated!",
+            ]
+        )
+    elif weapon == "newKnife":
+        playerChoice = fight_choice(
+            [
+                "As the "
+                + creatures
+                + " moves to attack, "
+                + "you unsheath your new newKnife.",
+                "The newKnife of Alexzandra the great shines "
+                + "brightly in your hand as you "
+                + "brace yourself for the attack.",
+                "But the pirate takes one look at your "
+                + "shiny new weapon and runs away!",
+                "You have rid the town of the "
+                + creatures
+                + ". "
+                + "You are victorious!",
+            ]
+        )
+    return playerChoice
+
+
+def cave(weapon):
+    if weapon == "badKnife":
+        print_pause("You peer cautiously into the cave.")
+        print_pause("It turns out to be only a very small cave.")
+        print_pause("Your eye catches a glint of metal behind a rock.")
+        print_pause("You have found the magical newKnife!")
+        print_pause("You discard your silly old dagger and take the sword "
+                    "with you.")
+        weapon = "newKnife"
+    elif weapon == "newKnife":
+        print_pause("You peer cautiously into the cave.")
+        print_pause("You've been here before, and gotten all the good stuff. "
+                    "It's just an empty cave now.")
+    print_pause("You walk back out to the field.\n")
+    return weapon
+
+
+def intro(creatures):
+    print_pause(
+        "You find yourself standing in an open field, filled with "
+        + "grass and yellow wildflowers."
+    )
+    print_pause(f"Rumor has it that a {creatures} is somewhere around here, "
+                "and has been terrifying the nearby village.")
+    print_pause("In front of you is a house.")
+    print_pause("To your right is a dark cave.")
+    print_pause("In your hand you hold your trusty "
+                "(but not very effective) dagger.\n")
+
+
+def play_game(creatures, weapon):
+    while True:
+        print_pause("Enter 1 to approach and knock on the door of the house.")
+        print_pause("Enter 2 to peer into the cave.")
+        print_pause("What would you like to do?")
+        output_option = valid_input("(Please enter 1 or 2).\n", ["1", "2"])
+        if output_option == "1":
+            playerChoice = house(creatures, weapon)
+            if playerChoice:
+                break
+        elif output_option == "2":
+            weapon = cave(weapon)
+
+
+def adventure_game():
+    weapon = "badKnife"
+    creaturesList = ["medusa", "pirate", "dinosaur", "wicked fairie"]
+    while True:
+        creatures = random.choice(creaturesList)
+        intro(creatures)
+        play_game(creatures, weapon)
         play_again()
 
-def play_game():
-    item = []
-    option = random.choice([f"medusa", "pirate", "dinosaur", "troll", "wicked fairie"])
-    intro(item, option)
-    field(item, option)
 
-
-if __name__ == '__main__':
-    play_game()
+if __name__ == "__main__":
+    adventure_game()
